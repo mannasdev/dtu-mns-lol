fb19940b2be8f14536d51202bc3174b72f0402da
Mannas <mannasnarang8@gmail.com>
2024-07-21 10:09:21 +0530

    push

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="text-2xl">Website Frontend :- <a class="underline text-blue-700" href="https://github.com/satyamjha148/clinkFrontEnd">https://github.com/satyamjha148/clinkFrontEnd</a></h1>
+<h1 class="text-2xl">Website Backend :- <a class="underline text-blue-700" href="https://github.com/mannasdev/click-backend">https://github.com/mannasdev/click-backend</a></h1>
+<h1>Canva PDF :- </h1>
