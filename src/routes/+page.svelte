09ee59f919d497160527749744c8a5c67e860477
Mannas <mannasnarang8@gmail.com>
2024-07-21 10:22:18 +0530

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<div class="m-5 flex flex-col gap-4">
+
 <h1 class="text-2xl">Website Frontend :- <a class="underline text-blue-700" href="https://github.com/satyamjha148/clinkFrontEnd">https://github.com/satyamjha148/clinkFrontEnd</a></h1>
 <h1 class="text-2xl">Website Backend :- <a class="underline text-blue-700" href="https://github.com/mannasdev/click-backend">https://github.com/mannasdev/click-backend</a></h1>
-<h1>Canva PDF :- </h1>
+<h1 class="text-2xl">Canva PPT :- <a class="underline text-blue-700" href="https://drive.google.com/file/d/1fP7axn-aAy4oQJf2lJ9BSlh-lesXCOWq/view?usp=sharing">Click here !</a></h1>
+
+</div>
